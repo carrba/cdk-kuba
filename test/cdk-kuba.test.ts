cc9679eac091d9945b0a3e7143e8655e74f3863b
Brian Carr <b.carr@btinternet.com>
2022-02-18 09:10:36 +0000

    Initial commit

---
+++ test/cdk-kuba.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkKuba from '../lib/cdk-kuba-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-kuba-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkKuba.CdkKubaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
