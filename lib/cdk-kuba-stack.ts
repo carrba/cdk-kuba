d82fbc7c8f1913e657c9b7bc199906a9401893d6
Brian Carr <b.carr@btinternet.com>
2022-03-08 17:17:37 +0000

    working version for new VPC

--- lib/cdk-kuba-stack.ts
+++ lib/cdk-kuba-stack.ts
@@ -6,7 +6,7 @@ import * as path from 'path';
 import { KeyPair } from 'cdk-ec2-key-pair';
 import { Subnet, Vpc } from 'aws-cdk-lib/aws-ec2';
 import * as iam from 'aws-cdk-lib/aws-iam'
-import {readFileSync} from 'fs';
+import {readFileSync, writeFileSync} from 'fs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export interface myStackProps extends StackProps {
@@ -23,7 +23,7 @@ export class CdkKubaStack extends Stack {
     if (props.vpcid) {
       myvpc = ec2.Vpc.fromLookup(this, "VPC", {
         vpcId: props.vpcid,
-        isDefault: false
+        //isDefault: false
       });
     }
     else {
@@ -38,16 +38,11 @@ export class CdkKubaStack extends Stack {
             subnetType: ec2.SubnetType.PUBLIC,
             cidrMask: 24,        
           },
-          {
-            name: 'private-subnet-1',
-            subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-            cidrMask: 24,
-          }
         ]
       });
     }
 
-    // ec2 security group
+    // ec2 web security group
     const webSG = new ec2.SecurityGroup(this, 'cdk-web-sg', {
       securityGroupName: 'cdk-web-sg',
       vpc: myvpc,
@@ -66,25 +61,50 @@ export class CdkKubaStack extends Stack {
       'allow incoming SSH'
     );
 
+    // ec2 no web security group
+    const nowebSG = new ec2.SecurityGroup(this, 'cdk-noweb-sg', {
+      securityGroupName: 'cdk-noweb-sg',
+      vpc: myvpc,
+      allowAllOutbound: true,
+    });
+
+    nowebSG.addIngressRule(
+      ec2.Peer.ipv4('77.100.137.249/32'),
+      ec2.Port.tcp(22),
+      'allow incoming SSH'
+    );
+    
+    nowebSG.addIngressRule(
+      ec2.Peer.ipv4('10.0.0.0/8'),
+      ec2.Port.tcp(80),
+      'allow 10.0.0.0/8 IP incoming HTTP'
+    );
+
+    nowebSG.addIngressRule(
+      ec2.Peer.ipv4('172.16.0.0/12'),
+      ec2.Port.tcp(80),
+      'allow 172.16.0.0/12 IP incoming HTTP'
+    );
+
+    nowebSG.addIngressRule(
+      ec2.Peer.ipv4('192.168.0.0/16'),
+      ec2.Port.tcp(80),
+      'allow 192.168.0.0/16 IP incoming HTTP'
+    );
+
     // key pair
     const keypair = new KeyPair (this, 'ec2-key', {
       name: 'ec2-keypair',
       publicKey: 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDECJsrrE4X19WGkg9VH4Z2LKaBfxTsKMRqq2wjDLqH15HRetG3DtDabWLXhJ+9gge5ImTftvyjOzgpx/CLCqTK72CrAN7TQIUZs8Dx/EJEIz8/+f8DWPwJ/VgbUj0of5ff3Q6eyDbV3Arhrar5uUbpvYnkQxouRIlLCk0MG9Z0qud+2zPZmrvtOnsKI1fdc53o68GYr1/ZiNmpH2JUsFBC4UOPou2s9uQlFzULOfksRbqQtivtvxhdokkKR2PoMvpTRhg9sdy5FoBxAZ13ZmGQRAJRsemBm0oKl7eSajxuxT3AQXkKAVkvQkue5Xz+2zEYVy1F5aqyStq17g4pPAtp5PuM2bUxoNrgMwQXjtfwfv2V+b5/RqwsQYPHiu0RH5qqbF8FVaKPP3AEVj4TIyOukcVSbwRDAawjvJe8Vl5JSnYlMATqXXE/UEVRt6oFP4yYZDf/nCPLAjkLHya6hlNpYDbtv8LThmKyOVfT4xdEjfvO5XzpOV4UTRqV9hqUINs= bcarr@Dell-Lat',
     })
-    /*
-    // ec2 role
-    const role = new iam.Role(this, 'ec2Role', {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
-    })
 
-    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'))
-    */
-    // ec2 public instance
-    const ec2public = new ec2.Instance(this, 'ec2-public', {
+    // ec2 box1 instance
+    const ec2box1 = new ec2.Instance(this, 'ec2-box1', {
       vpc: myvpc,
       vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC},
-      securityGroup: webSG,
+      securityGroup: nowebSG,
       keyName: keypair.keyPairName,
+      instanceName: "cdkkubabox1",
       // role: role,
       instanceType: ec2.InstanceType.of(
         ec2.InstanceClass.T2,
@@ -93,35 +113,44 @@ export class CdkKubaStack extends Stack {
       machineImage: ec2.MachineImage.latestAmazonLinux(),
     });
 
-    const ec2pip = new ec2.CfnEIP(this, "PIP")
-
-    const ec2pipassoc = new ec2.CfnEIPAssociation(this, "EIPAssoc", {
-      eip: ec2pip.ref,
-      instanceId: ec2public.instanceId
-    })
-    /*
-    // ec2 instance asset
-    const asset = new Asset(this, 'Asset', { path: path.join(__dirname, '../src/config.sh') });
-    const localpath = ec2public.userData.addS3DownloadCommand ({
-      bucket: asset.bucket,
-      bucketKey: asset.s3ObjectKey,
-    })
-    asset.grantRead(ec2public.role);
-    ec2public.userData.addExecuteFileCommand({
-      filePath: localpath,
-      arguments: '--verbose -y'
+    // ec2 box2 instance
+    const ec2box2 = new ec2.Instance(this, 'ec2-box2', {
+      vpc: myvpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC},
+      securityGroup: webSG,
+      keyName: keypair.keyPairName,
+      instanceName: "cdkkubabox2",
+      // role: role,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ec2.MachineImage.latestAmazonLinux(),
     });
-    */
+
+    let data = "sudo su -\n\nyum install httpd -y\nsleep 180\nTEXT=$(curl http://" + ec2box1.instancePrivateIp .trim() + ")\n"
+    + "echo -e \"$TEXT\" > /var/www/html/priv.html\nservice httpd start\nservice httpd enable\n";
+
     const userDataScript = readFileSync('./src/config.sh', 'utf8');
+    
+    writeFileSync('./src/priv2.sh', data);
+    const userDataScriptpriv = readFileSync('./src/priv2.sh', 'utf8');
 
     // 👇 add user data to the EC2 instance
-    ec2public.addUserData(userDataScript)
+    ec2box1.addUserData(userDataScript);
+    ec2box2.addUserData(userDataScriptpriv);
 
-    const myvpcOutput = new CfnOutput(this, "VPC-is", {
-      value:  myvpc.vpcId
+    const myvpcOutput = new CfnOutput(this, "VPC-id", {
+      value:  myvpc.vpcId,
+      description: 'VPC-ID'
     });
     const myec2ipOutput = new CfnOutput(this, "IP", {
-      value:  ec2public.instancePublicIp
+      value:  ec2box2.instancePublicIp,
+      description: 'Box 2 Public IP address'
+    });
+    const myec2iipOutput = new CfnOutput(this, "iIP", {
+      value:  ec2box1.instancePrivateIp,
+      description: 'Box 1 private IP address'
     });
   }
 }
--- src/config.sh
+++ src/config.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-
 sudo su -
 
 yum install httpd -y
 ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
-echo -e "$ID\n\n$IP" > /var/www/html/index.html
+IIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+echo -e "$ID\n\n$IP\n\n$IIP" > /var/www/html/index.html
 
 service httpd start
 service httpd enable
